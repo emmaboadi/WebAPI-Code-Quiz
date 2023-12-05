c9ad8385e8457302de74d06bcdb002a5ce707bf0
Jennifer Parsons <admin@Jennifer.local>
2023-12-05 19:13:08 +0000

    added Code Quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+const questions = [
+    {
+      question: "WWhat does the acronym DOM stand for in JavaScript?",
+      options: ["Document Object Model", "Data Object Model", "Document Order Model", "Dynamic Object Manipulation"],
+      correctAnswer: "Document Object Model"
+    },
+    {
+      question: "How do you declare a variable in JavaScript?",
+      options: ["var myVar", "let myVar", "const myVar", "All of the above"],
+      correctAnswer: "All of the above"
+    },
+    {
+        question: "What does the === operator do in JavaScript?",
+        options: ["Assigns a value to a variable", "Compares values for equality without type coercion", "Compares values for equality with type coercion", "Concatenates two strings"],
+        correctAnswer: "Compares values for equality without type coercion"
+      },
+    
+      {
+        question: "How do you include an external JavaScript file in an HTML document?",
+        options: ["<js src=script.js>", "<script href=script.js>", "<script src=script.js>","<link rel=script href=script.js>"],
+        correctAnswer: "<script src=script.js>"
+      },
+
+      {
+        question: "Which keyword is used to define a function in JavaScript?",
+        options: ["function", "def", "func", "method"],
+        correctAnswer: "function"
+      },
+    
+      {
+        question: "How can you comment a single line in JavaScript?",
+        options: ["// This is a comment", "/* This is a comment */", "# This is a comment", "' This is a comment"],
+        correctAnswer: "// This is a comment"
+      },
+];
